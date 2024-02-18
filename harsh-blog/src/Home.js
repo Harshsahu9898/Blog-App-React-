ee5c529936efdae4672b9060979e140124c19302
Harshsahu9898 <harshsahu9898@gmail.com>
2024-02-19 00:39:23 +0530

    added props

--- harsh-blog/src/Home.js
+++ harsh-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -23,12 +24,7 @@ const Home = () => {
     //     console.log('hello ' + name, e.target);
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key ={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs = {blogs} title= "All Blogs!" />
             {/* <h2>Homepage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button> */}
