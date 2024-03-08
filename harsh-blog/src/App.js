ffe5542b24988cd319e1f60c990b52f46dded1de
Harshsahu9898 <harshsahu9898@gmail.com>
2024-03-08 23:28:53 +0530

    added routes and create

--- harsh-blog/src/App.js
+++ harsh-blog/src/App.js
@@ -1,16 +1,18 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import './index.css';
-
+// import './index.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Create from './Create';
 function App() {
   // const title = 'welcome to the blog';
   // const likes = 50;
   // const link = "http://www.google.com";
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
-        <Home/>
+        {/* <Home/> */}
         {/* <h1>{title}</h1>
         <p>Liked {likes} times</p>
         <p>{64}</p>
@@ -18,8 +20,17 @@ function App() {
         <p>{[1,2,3,4,5]}</p>
         <p>Math.random() * 10</p>
         <a href="{link}">GOOGLE</a> */}
+        <Switch>
+          <Route exact path ="/">
+             <Home />
+          </Route>
+          <Route path ="/create">
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+  </Router>
   );
 }
 
