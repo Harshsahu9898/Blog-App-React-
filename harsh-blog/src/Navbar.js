048c4c3cdeb7c5edcc0233e375e48b6529ea9081
Harshsahu9898 <harshsahu9898@gmail.com>
2024-03-08 23:47:28 +0530

    added router links

--- harsh-blog/src/Navbar.js
+++ harsh-blog/src/Navbar.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Harsh Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                {/* <a href="/">Home</a>
                 <a href="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</a> */}
+                <Link to ="/">Home</Link>
+                <Link to ="/Create">New Blog</Link>
             </div>
         </nav>
      );
