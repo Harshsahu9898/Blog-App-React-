c39507f2da39973d15dc2163ed8e46af7f3ed706
Harshsahu9898 <harshsahu9898@gmail.com>
2024-03-31 23:34:24 +0530

    end->adding 404 page

--- harsh-blog/src/App.js
+++ harsh-blog/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './Notfound';
 function App() {
   // const title = 'welcome to the blog';
   // const likes = 50;
@@ -30,6 +31,9 @@ function App() {
           </Route>
           <Route path ="/blogs/:id">
             <BlogDetails/>
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
